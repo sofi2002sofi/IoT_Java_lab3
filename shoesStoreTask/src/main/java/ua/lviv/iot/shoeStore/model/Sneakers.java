964470f92a7ef722d761bf1d4142d1a089d3acae
Name Surname <info@developerS.com>
2020-03-10 23:11:05 +0200

    adding java lab 5

--- shoesStoreTask/src/main/java/ua/lviv/iot/shoeStore/model/Sneakers.java
+++ shoesStoreTask/src/main/java/ua/lviv/iot/shoeStore/model/Sneakers.java
@@ -1,12 +1,12 @@
 package ua.lviv.iot.shoeStore.model;
 
 public class Sneakers extends AbstractShoes {
-	
+
 	private boolean eyeletsPresence;
-	
+
 	private Specification specification;
 
-	public boolean isEyeletsPresence() {
+	public boolean getEyeletsPresence() {
 		return eyeletsPresence;
 	}
 
@@ -22,13 +22,21 @@ public class Sneakers extends AbstractShoes {
 		this.specification = specification;
 	}
 
-	public Sneakers(int sizeEURstandart, double priceInUAH, String assignment, Sex sex, String brand,
-			String color, String materialOfVamp, String materialOfLining, Heels heels, String toecapType,
-			boolean eyeletsPresence, Specification specification) {
+	public Sneakers(int sizeEURstandart, double priceInUAH, String assignment, Sex sex, String brand, String color,
+			String materialOfVamp, String materialOfLining, Heels heels, String toecapType, boolean eyeletsPresence,
+			Specification specification) {
 		super(sizeEURstandart, priceInUAH, assignment, sex, brand, color, materialOfVamp, materialOfLining, heels,
 				toecapType);
 		this.eyeletsPresence = eyeletsPresence;
 		this.specification = specification;
+	}
+
+	public String getHeaders() {
+		return super.getHeaders() + "," + "eyeletsPresence" + "," + "specification";
+	}
+
+	public String toCSV() {
+		return super.toCSV() + "," + getEyeletsPresence() + "," + getSpecification();
 	}
 
 }
